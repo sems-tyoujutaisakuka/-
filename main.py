8117fbb103388095de9a0f885ffef9b0bbdeaf47
sems-tyoujutaisakuka <machi342g@gmail.com>
2025-07-30 09:09:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def fetch_announcements():
 import requests as req_line
 
 LINE_TOKEN = 'cB46ZPwtJ5c2dj0zlBAJgU6KnjooopohcXUOb0PUiP9mPQ8evPWdKVVkKYHkwz5xT8Q9Ivg7m1ECOQE7/5Fm/3Ka1PwLAyPjGKhfRnZzYAR5eavFBxQ819jy1ir62vI7guCHMmn+2zEaKDDIralkhwdB04t89/1O/w1cDnyilFU='
-TO_USER_ID = 'Ucb45afffce2075e3b680490897dd2d87'
+TO_USER_ID = 'Cf28ceaa64690bf45ad9b0b5ece38d8d6'
 
 def send_line_message(message):
     headers = {
